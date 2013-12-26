e1890acbeb525342c1f5612e5005e2e77603ba97
Yu Yan <yyan@CNyyan.local>
2013-12-26 23:26:16 +0800

    Yan Yu: add baidu.com on home page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homework1::Application.config.secret_key_base = '92f59ef7d21d67a1eade1bf68bc665546edfddb8fbc69eca2fbf5b47c1dd2fd743b7bf010e91f56d8b4a769d38922914bcc8bbc68b30cb5c1fcceab0b4e30d71'
